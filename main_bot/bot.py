7940305378ab4fe38a1a6589d89463b24b63ad5e
羿潇贤鱼 <1025212779@qq.com>
2022-07-09 23:08:47 +0800

    缺失onebotv11组件

--- main_bot/bot.py
+++ main_bot/bot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 # Custom your logger
 # 
@@ -17,7 +18,9 @@ nonebot.init()
 app = nonebot.get_asgi()
 
 driver = nonebot.get_driver()
+driver.register_adapter(ONEBOT_V11Adapter)
 
+nonebot.load_builtin_plugins("echo")
 
 # Please DO NOT modify this file unless you know what you are doing!
 # As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
